41e7d71b49df3b230b108d2e45ae1b4de47db6fd
zhbname <zhbname@163.com>
2019-12-31 17:28:48 +0800

    Add files via upload
    进行设计模式工程的创建

---
+++ DesignPattern/src/main/java/demo/designpattern/mypattern/MypatternApplication.java
@@ -0,0 +1,13 @@
+package demo.designpattern.mypattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MypatternApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MypatternApplication.class, args);
+    }
+
+}
---
+++ DesignPattern/src/test/java/demo/designpattern/mypattern/MypatternApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.designpattern.mypattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MypatternApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
